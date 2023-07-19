36521da22af71615a62bcb71e25327e3f987f8eb
ecallegaris <emanuel.callegaris@hotmail.com>
2023-07-19 10:03:18 -0300

    ItemListContainer
    ItemListContainer

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ import React from 'react'
 // Importo NavBar.jsx llamando el componente mediante
 // <NavBar/>
 import NavBar from './componenets/NavBar'
+import ItemListContainer from './componenets/ItemListContainer'
 
 const App = () => {
   return (
     <>
       <NavBar/>
+      <ItemListContainer
+      greeting="Bienvenidos a mi tienda"
+      />
+      
     </>
   )
 }
